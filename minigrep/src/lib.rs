6a893929c4a22911fd977a12f17a3e68abea09aa
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-02-14 20:15:52 +0000

    adding library for minigrep and tests to follow

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,43 @@
+use std::{error::Error, fs};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if (args.len() < 3) {
+            return Err("Not enough arguments were provided.");
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use minigrep::Config;
+use std::{env::args, process};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to minigrep a rust implementation of grep");
+
+    let args: Vec<String> = args().collect();
+    let config = Config::build(&args[..]).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
