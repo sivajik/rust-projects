bc5db420b145989789c4ac5c6b154652d4ca9c47
Sivaji Kondapalli <sivaji.kondapalli@ianwilliams.co.uk>
2025-02-14 21:02:13 +0000

    redirect standard error using eprintln

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,11 +6,11 @@ fn main() {
 
     let args: Vec<String> = args().collect();
     let config = Config::build(&args[..]).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error: {e}");
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 }
